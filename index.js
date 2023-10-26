b351b5ba2e53bb1639a51a0900b16d7f1848b338
Chamuth Chamandana <c.chamandana@gmail.com>
2023-10-26 21:41:32 +0530

    add headers endpoint

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/ping', (req, res) => {
   res.send('Pong!')
 })
 
+app.get('/headers', (req, res) => {
+  // Get all request headers
+  const headers = req.headers;
+
+  // Send the headers as a JSON response
+  res.json(headers);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
